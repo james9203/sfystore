2ce57051e469761a7b1734c0a19f049854dbc400
luozhangqiao <james9203@126.com>
2019-09-24 11:54:53 +0800

    init project

---
+++ src/Base/Auth.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 11005884
+ * Date: 2019/9/24
+ * Time: 11:17
+ */
+namespace jamesluo\sfystore\Base;
+use jamesluo\sfystore\Http\Client;
+class Auth {
+    const AUTH_KEY = 'sfy_';
+    public function  __construct($AppId=null,$AppSecret=null,$merchantOpenId=null)
+    {
+    }
+
+}
